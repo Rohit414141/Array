47571d23f9e53249e879e70e9421ec9cb8c96746
Rohit414141 <95165510+Rohit414141@users.noreply.github.com>
2022-11-02 22:23:43 +0530

    Add files via upload

---
+++ AccendingEx.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class AccendingEx {
+
+	public static void main(String[] args) {
+		/*int marks[] = new int[] {10,3,50,1,26,500,4,30};
+		int temp;
+		for(int i=0;i<marks.length;i++) {
+			for(int j=i;j<marks.length;j++) {
+				if(marks[i]>marks[j]) {
+					temp=marks[i];
+					marks[i]=marks[j];
+					marks[j]=temp;
+				}
+				
+			}
+			//System.out.println(marks[i]);
+		}
+		for(int a:marks) {
+			System.out.println(a);
+		}
+		*/
+		
+		
+		
+	}
+}
